cd48f4dd94a3a58ada483b1dbbb172dd9c073a3e
beccahlen <beccahlenhart@gmail.com>
2023-08-02 11:51:15 -0400

    header and navbar beginning

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css'
+
+function Header () {
+    return (
+        <div className="header">Stranger's Things - sketchier than Craigslist</div>
+    )
+}
+
+export default Header
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import '../App.css'
+
+function NavBar () {
+    return(
+        <div className="navBar">
+            <a href='link'>Posts &nbsp;</a>
+            <a href='link'>Messages &nbsp;</a>
+            <a href='link'>Logout</a>
+        </div>
+    )
+}
+
+export default NavBar
